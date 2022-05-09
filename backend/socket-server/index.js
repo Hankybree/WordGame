07d38adfd0dd4dd2e90d10a9476612dbdf8e0346
Henrik Frank <frank.havsvinge@gmail.com>
2022-05-09 21:04:48 +0200

    Updated socket functions to do open, close and message

--- backend/socket-server/index.js
+++ backend/socket-server/index.js
@@ -1,27 +1,27 @@
 const { WebSocketServer } = require('ws')
 const commands = require('./commands')
 
-const clients = []
-
 const server = new WebSocketServer({ port: 8080 })
 
-server.on('connection', (socket) => {
-  socket.on('open', () => {
-    clients.push(socket)
-    console.log('socket connected')
-    socket.send('Connected!')
-  })
+server.on('connection', (socket, req) => {
+
+  acceptConnection(socket, req)
 
   socket.on('message', (data) => {
-    clients.forEach(client => {
+    console.log(data.toString())
+    server.clients.forEach(client => {
       client.send(data.toString())
     })
   })
 
   socket.on('close', () => {
-    clients.splice(clients.indexOf(socket), 1)
     console.log('Client disconnected')
   })
+
 })
+
+function acceptConnection(socket, req) {
+  console.log(`Client ${req.socket.remoteAddress} has connected`)
+}
 
 module.exports = server
