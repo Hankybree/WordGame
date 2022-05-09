72a8fca9c3eac35cb21a6c71e10f4c037288d1c1
Henrik Frank <frank.havsvinge@gmail.com>
2022-05-09 20:13:28 +0200

    Simple socket server. Does not work

--- backend/socket-server/commands.js
+++ backend/socket-server/commands.js
@@ -3,9 +3,17 @@ function joinGame() {
 }
 
 function playWord() {
-  
+
 }
 
 function leaveGame() {
 
 }
+
+const commands = new Map()
+
+commands.set('join-game', joinGame)
+commands.set('play-word', playWord)
+commands.set('leave-game', leaveGame)
+
+module.exports = commands
